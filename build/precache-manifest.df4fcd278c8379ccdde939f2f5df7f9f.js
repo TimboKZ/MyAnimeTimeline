a9cd9b9f0f897e71b66b831988fceaae58d9661e
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2020-04-07 21:08:15 +0100

    Add build

---
+++ build/precache-manifest.df4fcd278c8379ccdde939f2f5df7f9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad6db6392c3d285567728b82319dc60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ffdc2b26d11fdcb7a5c",
+    "url": "/static/css/main.f4fea000.chunk.css"
+  },
+  {
+    "revision": "5935f871bbc700f69ea6",
+    "url": "/static/js/2.1a4f8727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a4f8727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffdc2b26d11fdcb7a5c",
+    "url": "/static/js/main.1e3b3402.chunk.js"
+  },
+  {
+    "revision": "feb25eb2de995adebf09d7175d0eac51",
+    "url": "/static/js/main.1e3b3402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da502b7ab7d5641dbdee",
+    "url": "/static/js/runtime-main.aea66724.js"
+  }
+]);
